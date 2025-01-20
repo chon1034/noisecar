295f32bfd8a04e5a1ac0e4c84b7a9a55dd44efc5
chon1034 <chon1034@gmail.com>
2025-01-20 14:38:44 +0800

    Update mysql_pool_example.js

--- mysql_pool_example.js
+++ mysql_pool_example.js
@@ -1,65 +1,24 @@
-// 引入 MySQL 連接套件
-const mysql = require('mysql'); 
+var mysql = require("mysql");
 
-// 資料庫連線設定
-const dbConfig = {
-    host: 'ewk5w.h.filess.io',       // 資料庫主機的 IP 或主機名稱
-    user: 'noisecar_viewjobup',            // 資料庫帳號
-    password: '6aa2d7447c0cdbaa95ecddb873841d21af9ee310',     // 資料庫密碼
-    database: 'noisecar_viewjobup'        // 資料庫名稱
-};
+var hostname = "ewk5w.h.filess.io";
+var database = "noisecar_viewjobup";
+var port = "3307";
+var username = "noisecar_viewjobup";
+var password = "6aa2d7447c0cdbaa95ecddb873841d21af9ee310";
 
-// 定義連線資料庫的函數
-function connectdb() {
-    // 建立資料庫連線
-    const connection = mysql.createConnection(dbConfig);
-
-    // 嘗試連線並處理連線錯誤
-    connection.connect((err) => {
-        if (err) {
-            console.error('資料庫連線失敗：', err.message);
-            return;
-        }
-        console.log('成功連線到資料庫！');
-    });
-
-    return connection; // 回傳連線實例供後續操作
-}
-
-// 呼叫函數進行測試
-const dbConnection = connectdb();
-
-
-
-function updatedata(dataset, dbConnection){
-    dbConnection.query('UPDATE ?? SET Name = ? WHERE ID = ?', dataset, function(err, results){
-        if(err){
-            throw err;
-        }
-        console.log(results);
-    });
-    dbConnection.end();
-};
-
-d=['City','AW',1]
-conn1=connectdb()
-updatedata(d,conn1)
-
-
-// 測試查詢功能
-dbConnection.query('SELECT * FROM city where id = 1 LIMIT 5', (err, results, fields) => {
-    if (err) {
-        console.error('資料查詢失敗：', err.message);
-        return;
-    }
-    console.log('查詢結果：', results);
+var con = mysql.createConnection({
+  host: hostname,
+  user: username,
+  password,
+  database,
+  port,
 });
 
-// 關閉連線
-dbConnection.end((err) => {
-    if (err) {
-        console.error('關閉連線失敗：', err.message);
-        return;
-    }
-    console.log('成功關閉資料庫連線。');
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+con.query("SELECT 1+1").on("result", function (row) {
+  console.log(row);
 });
